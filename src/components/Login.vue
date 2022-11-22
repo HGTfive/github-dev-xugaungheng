aaec8f901a2b2bb54c72ab6933bb8dd0df291fb7
HGTfive <Kroal_xu@163.com>
2022-11-22 14:03:12 +0800

    Add files via upload
    完成初始化

---
+++ src/components/Login.vue
@@ -0,0 +1,135 @@
+<template>
+  <div id="login">
+    <div id="left">
+
+     
+
+    </div>
+    <div id="right">
+      <div id="ruleform">
+        
+        <div id="username">
+          <span>账号：</span>
+          <el-input v-model="username" placeholder="Please input username" />
+        </div>
+
+        <div id="password">
+          <span>密码：</span>
+          <el-input
+            v-model="password"
+            type="password"
+            placeholder="Please input password"
+            show-password
+          />
+        </div>
+
+        <div id="forget">
+          <span >
+            <a href="">忘记密码?</a>
+          </span>
+        </div>
+
+        <el-row class="mb-4">
+          <el-button type="success">登录</el-button>
+          <el-button id="default">注册</el-button>
+        </el-row>
+
+        
+        
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { reactive,ref } from "@vue/reactivity";
+export default {
+  name: "Login",
+  setup() {
+    // const userinfo = ref({
+    //   username: "",
+    //   password: "",
+    // });
+    const username = ref('');
+    const password = ref('');
+
+    let data = reactive([
+      require('../assets/qianduan.jpg'),
+      require('../assets/houduan.jpg'),
+      require('../assets/xinan.jpg'),
+      require('../assets/ios.jpg'),
+    ])
+    return {
+      username,
+      password,
+      data
+      // userinfo,
+    };
+  },
+};
+</script>
+
+<style lang="less" scoped>
+#login {
+  height: 100vh;
+  width: 100;
+  background-color: #fff;
+  display: flex;
+  flex-direction: row;
+}
+#left {
+  flex: 1;
+  background-color: #fff;
+ 
+}
+#right {
+  flex: 1;
+}
+#ruleform{
+  width: 400px;
+  height: 350px;
+  margin: 0 auto;
+  margin-left: 50px;
+  border: 1px solid rgba(43, 41, 41, 0.5); 
+  border-radius: 6px;
+  margin-top: 200px;
+  background-color: #fbf3f3;
+  #username{
+    width: 70%;
+    margin: 0 auto;
+    display: flex;
+    margin-top: 80px;
+    .el-input{
+      width: 80%;
+    }
+
+  }
+  #password{
+    width: 70%;
+    margin: 0 auto;
+    display: flex;
+    margin-top: 30px;
+    .el-input{
+      width: 80%;
+    }
+  }
+  .el-row{
+    width: 70%;
+    margin: 0 auto;
+    margin-left: 110px;
+    margin-top: 50px;
+    #default{
+      margin-left: 60px;
+    }
+  }
+  #forget{
+    float: right;
+
+    // margin-top: 10px;
+    margin-right: 70px;
+    font-size: 14px;
+
+    // margin-bottom: 4px;
+  }
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <Login></Login>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Login from '../components/Login.vue' // @ is an alias to /src
+
+export default defineComponent({
+  name: 'Home',
+  components: {
+    Login
+  }
+})
+</script>
